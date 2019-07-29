d96c733b5aecb686e4d14592518addd5c7f30f48
phanisai <saikumar.chapparam@hexagonsi.com>
2019-07-29 12:37:38 +0530

    Sample screen and structure is added with navigation

---
+++ RNTraining/src/HomeScreen.js
@@ -0,0 +1,59 @@
+import React, { Fragment } from 'react';
+import {
+    StyleSheet,
+    View,
+    Text,
+    FlatList,
+    Button,
+} from 'react-native';
+
+const topicList = [
+    {key: 'Components'},
+    {key: 'Styles'},
+    {key: 'FlexBox'},
+    {key: 'List'},
+    {key: 'Redux'},
+    {key: 'NativeModules'},
+    {key: 'WebApis'},
+    {key: 'debugging'},
+  ]
+  
+export default class HomeScreen extends React.Component {
+    static navigationOptions = {
+        title: 'Welcome',
+    };
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <Fragment>
+                <View style={styles.container}>
+                    <FlatList
+                        data={topicList}
+                        renderItem={({ item }) => <Button
+                           title = {item.key}
+                           style={styles.item}
+                            onPress={() => navigate(item.key, {})}
+                        /> }
+                    />
+                </View>
+
+            </Fragment>
+
+        );
+    }
+}
+
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingTop: 22
+    },
+    item: {
+        padding: 10,
+        fontSize: 18,
+        height: 44,
+        alignContent: 'center',
+    }
+});
